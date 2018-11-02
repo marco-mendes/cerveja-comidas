74a429e7ead480973e0e2dda2a9b5f1a59ce3122
corelioBH <corelio@gmail.com>
2018-11-02 19:56:39 -0300

    Importacao inicial da aplicacao

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,8 +7,29 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+    cerveja = '';
+    comida = '';
 
-  }
+    private combinacoes = 
+    {
+      "Pale Ale" : ["Pizza Calabresa", "Churrasco", "Salgadinhos de Pacote"],
+      "Lager" : ["Pizza Margerita", "Salgadinhos de Pacote"],
+      "Pilsen" : ["Asinha de Frango", "Fettucine Alfredo", "Batata Frita", "Linguiça apimentada"],
+      "IPA" : ["Churrasco de picanha", "Torresmo", "Chessbacon", "Feijoada Gorda"],
+      "Red Ale" : ["Churrasco", "Frango Assado"],
+      "Bock" : ["Costela de porco grelhada", "Lombo de porco assado", "Javali"],
+      "Weiss (trigo)" : ["Muqueca", "Peixe Assado", "Ceviche"]
+    }
+
+
+    private cervejasUnicas = Object.keys(this.combinacoes);
+
+
+   
+    get combinacao() {   
+      return this.combinacoes[this.cerveja];
+    } 
+
+
 
 }
